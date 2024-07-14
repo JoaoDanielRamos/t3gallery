4676202f6a26dfab1b14365ea903816be9416376
JoaoDanielRamos <jdmor95@gmail.com>
2024-07-13 21:52:28 -0700

    scafold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+const mockUrls = [
+  "https://utfs.io/f/17981bb8-1ca3-449b-8c38-05b7cf30fbad-ri0xv3.jpg",
+  "https://utfs.io/f/1c20eda5-ac94-4d84-ab98-2b1eaaa33d73-khd7eq.png",
+  "https://utfs.io/f/06ec6c9e-47fa-4d78-a799-a5cccdd5ea99-39zaak.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center text-white">
+      <div className="flex flex-wrap">
+        {mockImages.map((image) => (
+          <div key={image.id}>
+            <img src={image.url} className="w-1/2 p-4" />
+          </div>
+        ))}
+      </div>
       <p>gallery in progress</p>
     </main>
   );
