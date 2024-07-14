afbb2ff3e133f03b7635cd0696ab90f64166ba1a
JoaoDanielRamos <jdmor95@gmail.com>
2024-07-13 22:28:37 -0700

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/17981bb8-1ca3-449b-8c38-05b7cf30fbad-ri0xv3.jpg",
   "https://utfs.io/f/1c20eda5-ac94-4d84-ab98-2b1eaaa33d73-khd7eq.png",
@@ -9,10 +11,16 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white">
       <div className="flex flex-wrap">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id}>
             <img src={image.url} className="w-1/2 p-4" />
